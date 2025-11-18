936c8ca6bf2b6296555dc1236b14657d26a6629e
deepak-neebhatech <deepak@neebhatech.com>
2025-11-18 17:37:19 +0530

    New code add

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Namaste React 🎯
+
+# Parcel
+-Dev Build
+-Local Server
+-HMR = Hot Module Replacement
+-File Watching Algorithm - written in c++
+-Caching - Faster Builds
--- app.js
+++ app.js
@@ -1,35 +1,29 @@
-// const heading = React.createElement('h1', {}, "Hello Javascript");
-// const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// root.render(heading);
-
-// parent div
 const parent = React.createElement(
     "div",
     { id: "parent" },
     [
-        // child1
         React.createElement(
             "div",
-            { id: "child1" },
+            { id: "child1", key: "child1" },             // <-- key added
             [
-                React.createElement("h1", { id: "heading_1" }, "Heading 1"),
-                React.createElement("h2", { id: "heading_2" }, "Heading 2")
+                React.createElement("h1", { id: "h1_1", key: "h1_1" }, "Heading 1"),
+                React.createElement("h2", { id: "h2_1", key: "h2_1" }, "Heading 2"),
             ]
         ),
 
-        // child2
         React.createElement(
             "div",
-            { id: "child2" },
+            { id: "child2", key: "child2" },             // <-- key added
             [
-                React.createElement("h1", { id: "heading_1" }, "Heading 1"),
-                React.createElement("h2", { id: "heading_2" }, "Heading 2")
+                React.createElement("h1", { id: "h1_2", key: "h1_2" }, "Heading 1"),
+                React.createElement("h2", { id: "h2_2", key: "h2_2" }, "Heading 2"),
             ]
-        )
+        ),
     ]
 );
 
-// Render
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
